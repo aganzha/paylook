67e69bc07e09e48348248f36af490ac01c90701d
Artur <morozarturv@gmail.com>
2013-03-05 18:14:06 +0200

    nav without pictures

--- js/common.js
+++ js/common.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
 
+//form nav
+$('.form-nav a').click(function(){
+	if (!$(this).hasClass('active') && !$(this).hasClass('disabled')) {
+		$('.form-nav a').removeClass('active');
+		$('.form-nav a').removeClass('prev-active');
+		$(this).addClass('active');
+	};
+})
 
 });
