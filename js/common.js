7e0ce10b6aaf42a23452df2116163bc807eb1a44
Artur <morozarturv@gmail.com>
2013-03-14 14:28:50 +0200

    done

--- js/common.js
+++ js/common.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
 
+var current = new Date();
+  if (current.getMonth() == 0){
+    current.setMonth(11);
+    current.setFullYear(current.getFullYear()-1);
+  } else {
+    current.setMonth(current.getMonth()-1);
+  }
+
+$('.form__datepicker').DatePicker({
+  flat: true,
+  format: 'd B',
+  date: [],
+  current: current,
+  calendars: 3,
+  mode: 'range',
+  starts: 1,
+  onChange: function(formated) {
+    $('.form__period').get(0).innerHTML = '<b>' + formated.join('</b> – <b>') + '</b>';
+  }
+});
+
+
 });
